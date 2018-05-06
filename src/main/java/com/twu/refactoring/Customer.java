21f174d4cdec07ba2c7dbec162c6c7b0f698c747
Chongfa <cfachen@thoughtworks.com>
2018-05-06 12:09:00 +0800

    [Chongfa] #n/a implement htmlStatement

--- src/main/java/com/twu/refactoring/Customer.java
+++ src/main/java/com/twu/refactoring/Customer.java
@@ -53,12 +53,21 @@ public class Customer {
     }
 
     public String htmlStatement() {
-        return "<H1>Rentals for <EM>Dinsdale Pirhana</EM></H1><P>" +
-                "Monty Python and the Holy Grail: 3.5<BR>" +
-                "Ran: 2.0<BR>LA Confidential: 6.0<BR>" +
-                "Star Trek 13.2: 3.0<BR>" +
-                "Wallace and Gromit: 6.0<BR><P>" +
-                "You owe <EM>20.5</EM><P>" +
-                "On this rental you earned <EM>6</EM> frequent renter points<P>";
+        return htmlHead() + htmlBody() + htmlFoot();
+    }
+
+    private String htmlFoot() {
+        return "<P>You owe <EM>" + getTotalAmount() + "</EM><P>" +
+                "On this rental you earned <EM>" + getFrequentRenterPoints() + "</EM> frequent renter points<P>";
+    }
+
+    private String htmlHead() {
+        return "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>";
+    }
+
+    private String htmlBody() {
+        return rentalList.stream()
+                         .map(rental -> rental.getMovie() .getTitle() + ": " + String.valueOf(rental.getCharge()) + "<BR>")
+                         .collect(Collectors.joining(""));
     }
 }
