58bc80095caa6f3b36ae45528e3e099ced683754
Chongfa <cfachen@thoughtworks.com>
2018-05-06 11:40:07 +0800

    [Chongfa] #n/a extract getBody method

--- src/main/java/com/twu/refactoring/Customer.java
+++ src/main/java/com/twu/refactoring/Customer.java
@@ -2,6 +2,7 @@ package com.twu.refactoring;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.stream.Collectors;
 
 public class Customer {
 
@@ -21,20 +22,19 @@ public class Customer {
     }
 
     public String statement() {
-        Iterator<Rental> rentals = rentalList.iterator();
         String result = "Rental Record for " + getName() + "\n";
-        while (rentals.hasNext()) {
-            Rental each = rentals.next();
-
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t"
-                    + String.valueOf(each.getCharge()) + "\n";
-        }
+        result += getBody();
         // add footer lines
         result += "Amount owed is " + String.valueOf(getTotalAmount()) + "\n";
         result += "You earned " + String.valueOf(getFrequentRenterPoints())
                 + " frequent renter points";
         return result;
+    }
+
+    private String getBody() {
+        return rentalList.stream()
+                         .map(rental -> "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n")
+                         .collect(Collectors.joining(""));
     }
 
     private int getFrequentRenterPoints() {
