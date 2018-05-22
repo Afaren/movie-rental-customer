a917c7996804bb9d0ac9ed6dfa46a2bb8832806b
Chongfa <cfachen@thoughtworks.com>
2018-05-22 12:34:48 +0800

    extract getTotalAmount method

--- src/main/java/com/twu/refactoring/Customer.java
+++ src/main/java/com/twu/refactoring/Customer.java
@@ -21,14 +21,11 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Iterator<Rental> rentals = rentalList.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
 			Rental each = rentals.next();
-
-			double thisAmount = amountFor(each);
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -39,15 +36,21 @@ public class Customer {
 
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+					+ String.valueOf(amountFor(each)) + "\n";
+
 
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalAmount()) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	private double getTotalAmount() {
+		return rentalList.stream()
+						 .mapToDouble(this::amountFor)
+						 .sum();
 	}
 
 	private double amountFor(Rental each) {
